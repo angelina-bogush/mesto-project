b8db29e62f4a539516e9b47e1f8d042c35e890dc
Angelina Bogush <angelinabogush@yandex.ru>
2023-04-19 18:38:41 +0300

    начала делать форму

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let popup = document.querySelector('.popup');
 editButton.addEventListener('click', function openPopup() {
     popup.classList.add('popup_opened');
 });
+let closeIcon = popup.querySelector('.popup__close-icon');
+closeIcon.addEventListener('click', function closePopup() {
+    popup.classList.remove('popup_opened');
+});
+/*form*/
+const form = document.querySelector('.form');
+const formName = form.querySelector('.')
+const formDescription
