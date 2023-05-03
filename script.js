521245ae827b0c9f8ee82e5bdb16e204f7d863f9
Angelina Bogush <angelinabogush@yandex.ru>
2023-05-03 17:21:39 +0300

    сделан попап картинки

--- script.js
+++ script.js
@@ -7,112 +7,154 @@ const editButton = profile.querySelector('.profile__edit-button'); // кнопк
 const popup = page.querySelector('.popup');
 const popupEditProfile = page.querySelector('.popup_theme_edit'); //попап редактирования профиля
 const popupAddCard = page.querySelector('.popup_theme_add-card');
+//const popupPhoto = page.querySelector('.popup_theme_photo');
 const addButton = profile.querySelector('.profile__add-button'); //кнопка добавления карточки
 const closeIconAdd = page.querySelector('.popup__close-icon_add-card');  // закрытие окна добавления карточки
 const closeIcon = popup.querySelector('.popup__close-icon'); // общяя кнопка закрытия окна (?)
 const closeIconEdit = popup.querySelector('.popup__close-icon_edit');  // закрытие окна редактирования профиля
+const closeIconPhoto = page.querySelectorAll('.popup__close-icon_photo');
 
-function openPopup(popupName) {                                   //функция открыть попап
-    popupName.classList.add('popup_opened');}
-
-editButton.addEventListener('click', () => {
-    openPopup(popupEditProfile);
-});
-addButton.addEventListener('click', () => {
-    openPopup(popupAddCard)});
-
+function openPopup(popupName) {
+  //открыть и закрыть попап функции
+  popupName.classList.add("popup_opened");
+}
 function closePopup(popupName) {
-    popupName.classList.remove('popup_opened')};     
-                                                      //функция закрыть попап
- closeIconEdit.addEventListener('click', () => {
-    closePopup(popupEditProfile)});
- closeIconAdd.addEventListener('click', () => {
-    closePopup(popupAddCard)});
+  popupName.classList.remove("popup_opened");
+}
+
+editButton.addEventListener("click", () => {
+  openPopup(popupEditProfile);
+});
+addButton.addEventListener("click", () => {
+  openPopup(popupAddCard);
+});
+closeIconEdit.addEventListener("click", () => {
+  closePopup(popupEditProfile);
+});
+closeIconAdd.addEventListener("click", () => {
+  closePopup(popupAddCard);
+});
  
 
 //изменение имени в форме редактирования профиля
 //const form = document.querySelector('.form');
-const formEdit = page.querySelector('.form_edit');
-const formName = formEdit.querySelector('.form__input_theme_name');
-const formDescription = formEdit.querySelector('.form__input_theme_description');
-const buttonSubmit = formEdit.querySelector('.form__button_submit');
-const profileTitle = profile.querySelector('.profile__title');
-const profileSubtitle = profile.querySelector('.profile__subtitle');
+const formEdit = page.querySelector(".form_edit");
+const formName = formEdit.querySelector(".form__input_theme_name");
+const formDescription = formEdit.querySelector(
+  ".form__input_theme_description"
+);
+const buttonSubmit = formEdit.querySelector(".form__button_submit");
+const profileTitle = profile.querySelector(".profile__title");
+const profileSubtitle = profile.querySelector(".profile__subtitle");
 
-buttonSubmit.addEventListener('click', function submitForm(evt){
-    evt.preventDefault();
-    profileTitle.textContent = formName.value;
-    profileSubtitle.textContent = formDescription.value;
-    popupEditProfile.classList.remove('popup_opened');
-    });
+buttonSubmit.addEventListener("click", function submitForm(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = formName.value;
+  profileSubtitle.textContent = formDescription.value;
+  popupEditProfile.classList.remove("popup_opened");
+});
 
 
   //массив из содержимого карточек
 
     const contentCards = [
-        {
-            name: 'Петергоф',
-          link: 'https://images.unsplash.com/photo-1635700453672-8e93c5f2e280?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
-        },
-        {
-          name: 'Исаакиевский Собор',
-          link: 'https://images.unsplash.com/photo-1555460285-763ba96917d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-        },
-        {
-          name: 'Лахта центр',
-          link: 'https://images.unsplash.com/photo-1539203645471-3ae5d31e0eca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80'
-        },
-        {
-          name: 'Казанский собор',
-          link: 'https://images.unsplash.com/photo-1551005756-fd0657e8fbf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2FpbnQlMjBwZXRlcnNidXJnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-        },
-        {
-          name: 'Невский проспект',
-          link: 'https://images.unsplash.com/photo-1626103254920-d8c4069554da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTl8fHNhaW50JTIwcGV0ZXJzYnVyZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
-        },
-        {
-          name: 'Пушкин',
-          link: 'https://images.unsplash.com/photo-1573551673739-6075df0508c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDl8fHNhaW50JTIwcGV0ZXJzYnVyZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
-        }
-      ];
+      {
+        name: "Петергоф",
+        link: "https://images.unsplash.com/photo-1635700453672-8e93c5f2e280?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+      },
+      {
+        name: "Исаакиевский Собор",
+        link: "https://images.unsplash.com/photo-1555460285-763ba96917d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      },
+      {
+        name: "Лахта центр",
+        link: "https://images.unsplash.com/photo-1539203645471-3ae5d31e0eca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
+      },
+      {
+        name: "Казанский собор",
+        link: "https://images.unsplash.com/photo-1551005756-fd0657e8fbf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2FpbnQlMjBwZXRlcnNidXJnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      },
+      {
+        name: "Невский проспект",
+        link: "https://images.unsplash.com/photo-1626103254920-d8c4069554da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTl8fHNhaW50JTIwcGV0ZXJzYnVyZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      },
+      {
+        name: "Пушкин",
+        link: "https://images.unsplash.com/photo-1573551673739-6075df0508c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDl8fHNhaW50JTIwcGV0ZXJzYnVyZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      },
+    ];
 
 //Создание карточки
-const cardContainer = page.querySelector('.cards');
-const cardTemplate = page.querySelector('#template-card').content; //добавили template
-const createCard = function(cardName, cardLink){
-    const newCard = cardTemplate.querySelector('.card').cloneNode(true);  //клонировали карточку
-    const newCardName = newCard.querySelector('.card__title'); // Название карточки
-    const newCardImage = newCard.querySelector('.card__image');  // картинка карточки
-    newCardName.textContent = cardName; // добавление заголовка карточки
-    newCardImage.src = cardLink;  //ссылка на картинку карточки (значение из формы)
-    newCardImage.alt = cardName;
-    return newCard;
+const cardContainer = page.querySelector(".cards");
+const cardTemplate = page.querySelector("#template-card").content; //добавили template
+const createCard = function (cardName, cardLink) {
+  const newCard = cardTemplate.querySelector(".card").cloneNode(true); //клонировали карточку
+  const newCardName = newCard.querySelector(".card__title"); // Название карточки
+  const newCardImage = newCard.querySelector(".card__image"); // картинка карточки
+  newCardName.textContent = cardName; // добавление заголовка карточки
+  newCardImage.src = cardLink; //ссылка на картинку карточки (значение из формы)
+  newCardImage.alt = cardName;
+  return newCard;
 };
-const addCard = function(cardName, cardLink) {
+
+
+const popupPhoto = page.querySelector('.popup_theme_photo');
+const popupPhotoImage = popupPhoto.querySelector('.popup__image');
+const popupPhotoDescription = popupPhoto.querySelector('.popup__description');
+const addCard = function (cardName, cardLink) {
   const newCard = createCard(cardName, cardLink);
-  const cardLike = newCard.querySelectorAll('.card__like');
-  const buttonTrash = newCard.querySelectorAll('.card__trash');
-  const cardImage = newCard.querySelectorAll('.card__image');
-  /*cardImage.forEach((item) => item.addEventListener('click', () => {
-    openPopup(...)
-  })*/
-  /*cardLike.addEventListener('click', function addClass(){
-    cardLike.classList.toggle('card__like_active');          
-      });*/
-  cardLike.forEach((item) => item.addEventListener('click', function addClass(evt){
-  const eventTarget = evt.target;
-  eventTarget.classList.toggle('card__like_active');           //лайк
-      }));
+  const cardLike = newCard.querySelectorAll(".card__like");
+  const buttonTrash = newCard.querySelectorAll(".card__trash");
+  const cardImage = newCard.querySelectorAll(".card__image");
+  
+const cardClick = function(){                                        // открытие попапа картинки
+  popupPhotoImage.src = cardLink;
+  popupPhotoImage.alt = cardLink;
+  popupPhotoDescription.textContent = cardName;
+  openPopup(popupPhoto);
+}
 
-  buttonTrash.forEach((item) => item.addEventListener('click', function deleteCard(){
-      const cardToDelete = document.querySelector('.card');
-      cardToDelete.remove();                                  //удаление карточки
-     }));
+cardImage.forEach((item) =>
+    item.addEventListener("click", () => {
+      cardClick();                         
+    })
+  );
+  closeIconPhoto.forEach((item) =>
+    item.addEventListener("click", () => {
+      closePopup(popupPhoto);
+    })
+  );
+  /*cardImage.forEach((item) =>
+    item.addEventListener("click", () => {
+      openPopup(popupPhoto);    
+                              
+    })
+  );
+  closeIconPhoto.forEach((item) =>
+    item.addEventListener("click", () => {
+      closePopup(popupPhoto);
+    })
+  );*/
+  cardLike.forEach((item) =>
+    item.addEventListener("click", function addClass(evt) {
+      const eventTarget = evt.target;
+      eventTarget.classList.toggle("card__like_active");       //лайк
+    })
+  );
 
-  cardContainer.prepend(newCard)};
+  buttonTrash.forEach((item) =>
+    item.addEventListener("click", function deleteCard() {
+      const cardToDelete = document.querySelector(".card");
+      cardToDelete.remove();                            //удаление карточки
+    })
+  );
 
-contentCards.forEach(info => {
-  addCard(info.name, info.link)})
+  cardContainer.prepend(newCard);
+};
+
+contentCards.forEach((info) => {
+  addCard(info.name, info.link);
+});
 
   /*const cardLike = document.querySelectorAll('.card__like');
   cardLike.forEach((item) => item.addEventListener('click', function addClass(evt){
@@ -137,4 +179,3 @@ contentCards.forEach(info => {
      
 //открытие просмотра фотографий (открытие попапа)
 
-//const popupPhoto
